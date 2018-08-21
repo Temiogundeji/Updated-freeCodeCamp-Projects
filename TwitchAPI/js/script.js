536c024bfd2372da6df438b448fa7873e0dedeee
Temi Ogundeji <yusuffogundeji@gmail.com>
2018-08-21 08:13:34 +0100

    Twitch users data requested is displayed

--- TwitchAPI/js/script.js
+++ TwitchAPI/js/script.js
@@ -17,41 +17,57 @@ function getStreamersInfo(){
 
              //Return the display name, status and logo
             $.getJSON(streamUrl, function(data){
-                var game, status;
-            if(data.stream == null){
-                game = "offline";
-                status = "offline";
-                displayData(name, game, logo,status, description, url);
-            }
-            else if(data.stream == undefined){
-                game = "Account closed";
-                status = "offline";
-                displayData(name, game, logo,status, description, url);
-            }
-            else{
-                game = data.stream.game;
-                status = "online";
-                displayData(name, game, logo,status, description, url);
-            }
+                    var game, status;
+                if(data.stream == null){
+                    game = "offline";
+                    status = "offline";
+                    displayData(name, game, logo,status, description, url);
+                }
+                else if(data.stream == undefined){
+                    game = "Account closed";
+                    status = "offline";
+                    displayData(name, game, logo,status, description, url);
+                }
+                else{
+                    game = data.stream.game;
+                    status = "online";
+                    displayData(name, game, logo,status, description, url);
+                }
 
             });
         });  
     });
 }
+function filterData(name, game, logo, status, description, url){
+    
+    var value = document.getElementById('statuss').value;
+    // var value = select.options[select.selectedIndex].value;
+
+     if(value == "online"){
+        status.filter(el => el == "online");
+    }
+    else if(value == "offline"){
+        status.filter(el => el == "offline");
+    }
+    else{
+        displayData(name, game, logo, status, description, url);
+    }
+}
 
 function displayData(name, game, logo,status, description, url){
-    var display = `
-    <div>
-        <a href="${url}" target="_blank">
-            <img src="${logo}">
-            <span>${status}</span>
-            <span>${game}</span>
-            <span>${description}
-        </a>
-    </div>
-`
-$('#result').html(display);
-console.log(name);
+     var display = `
+      <div>
+          <a href="${url}" target="_blank">
+              <img id="logo" src="${logo}">
+              <span>${name}</span>
+              <span>${status}</span>
+              <span>${game}</span>
+              <span>${description}
+          </a>
+      </div>
+  `;
+  $('#result').append(display); 
+
 }
 
 $(document).ready(function(){
